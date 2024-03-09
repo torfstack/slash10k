b43dbaecf986dd3d50721f475bdbb8c50ae19c3b
Max Torf <torfstack@gmail.com>
2024-03-09 16:55:40 +0100

    added 10kchannel cmd

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"scurvy10k/internal/models"
+	"scurvy10k/internal/command"
 	"strings"
 
 	"github.com/diamondburned/arikawa/v3/discord"
@@ -18,7 +14,6 @@ import (
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/state"
-	"github.com/diamondburned/arikawa/v3/utils/json/option"
 )
 
 var commands = []api.CreateCommandData{
@@ -27,16 +22,10 @@ var commands = []api.CreateCommandData{
 		&discord.StringOption{OptionName: "name", Description: "Name des Spielers", Required: true},
 		&discord.StringOption{OptionName: "amount", Description: "Betrag, kann negativ sein", Required: true},
 	}},
-	{Name: "10kup", Description: "Setze den Channel für 10k updates", Options: discord.CommandOptions{
+	{Name: "10kchannel", Description: "Setze den Channel für 10k updates", Options: discord.CommandOptions{
 		&discord.ChannelOption{OptionName: "channel_id", Description: "Channel für 10k updates", Required: true},
 	}},
 }
-
-var baseUrl = "https://true.torfstack.com/"
-var debtsUrl = baseUrl + "api/debt"
-
-var channelId discord.ChannelID
-var messageId discord.MessageID
 
 func main() {
 	setupLogger()
@@ -48,67 +37,9 @@ func main() {
 	s.AddIntents(gateway.IntentGuilds)
 	s.AddIntents(gateway.IntentMessageContent)
 
-	r.AddFunc("10ks", func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
-		debts, err := getDebts()
-		if err != nil {
-			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + err.Error())}
-		}
-		return &api.InteractionResponseData{
-			Embeds: &[]discord.Embed{
-				*transformDebtsToEmbed(debts),
-			},
-		}
-	})
-
-	r.AddFunc("10k", func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
-		options := data.Options
-		name := options.Find("name").String()
-		amount := options.Find("amount").String()
-
-		res, err := http.Post(debtsUrl+"/"+name+"/"+amount, "application/json", nil)
-		if err != nil {
-			log.Error().Msgf("cannot post debt: %s", err)
-			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + err.Error())}
-		}
-		defer res.Body.Close()
-		if res.StatusCode != http.StatusOK {
-			log.Error().Msgf("cannot post debt: %s", res.Status)
-			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + res.Status)}
-		}
-		if channelId != discord.NullChannelID && messageId != discord.NullMessageID {
-			updateDebtsMessage(s)
-		}
-		return &api.InteractionResponseData{
-			Content: option.NewNullableString(fmt.Sprintf("Added %v to %v", amount, name)),
-			Flags:   discord.EphemeralMessage,
-		}
-	})
-
-	r.AddFunc("10kup", func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
-		options := data.Options
-		var err error
-		cId, err := options.Find("channel_id").SnowflakeValue()
-		if err != nil {
-			log.Error().Msgf("cannot get channel_id: %s", err)
-			return &api.InteractionResponseData{Content: option.NewNullableString("Could not set channel")}
-		}
-		channelId = discord.ChannelID(cId)
-		debts, err := getDebts()
-		if err != nil {
-			log.Error().Msgf("cannot get debts: %s", err)
-			return &api.InteractionResponseData{Content: option.NewNullableString("Could not get debts")}
-		}
-		m, err := s.SendMessage(discord.ChannelID(channelId), "", *transformDebtsToEmbed(debts))
-		if err != nil {
-			log.Error().Msgf("cannot send message: %s", err)
-			return &api.InteractionResponseData{Content: option.NewNullableString("Could not send message")}
-		}
-		messageId = m.ID
-		return &api.InteractionResponseData{
-			Content: option.NewNullableString("Channel set successfully"),
-			Flags:   discord.EphemeralMessage,
-		}
-	})
+	r.AddFunc("10ks", command.GetDebts(s))
+	r.AddFunc("10k", command.AddDebt(s))
+	r.AddFunc("10kchannel", command.SetChannel(s))
 
 	if err := cmdroute.OverwriteCommands(s, commands); err != nil {
 		log.Fatal().Msgf("cannot update commands: %s", err)
@@ -135,61 +66,4 @@ func setupLogger() {
 	}
 	output := zerolog.ConsoleWriter{Out: os.Stderr}
 	log.Logger = log.Output(output)
-}
-
-func getDebts() (*models.AllDebtsResponse, error) {
-	res, err := http.Get(debtsUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	bytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	var debts models.AllDebtsResponse
-	if err = json.Unmarshal(bytes, &debts); err != nil {
-		log.Error().Msgf("cannot unmarshal debts: %s", err)
-		return nil, err
-	}
-	return &debts, nil
-}
-
-func transformDebtsToEmbed(debts *models.AllDebtsResponse) *discord.Embed {
-	var fields []discord.EmbedField
-	for _, d := range debts.Debts {
-		fields = append(fields, discord.EmbedField{
-			Name:   d.Name,
-			Value:  d.Amount,
-			Inline: true,
-		})
-	}
-	return &discord.Embed{
-		Title:       "True",
-		Type:        discord.NormalEmbed,
-		Description: "10k in die Gildenbank!",
-		URL:         baseUrl,
-		Timestamp:   discord.NowTimestamp(),
-		Color:       discord.DefaultEmbedColor,
-		Footer: &discord.EmbedFooter{
-			Text: "https://github.com/torfstack/scurvy10k",
-			Icon: "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
-		},
-		Fields: fields,
-	}
-}
-
-func updateDebtsMessage(s *state.State) {
-	debts, err := getDebts()
-	if err != nil {
-		log.Error().Msgf("cannot get debts: %s", err)
-		return
-	}
-
-	m, err := s.EditMessage(channelId, messageId, "", *transformDebtsToEmbed(debts))
-	if err != nil {
-		log.Error().Msgf("cannot edit message: %s", err)
-		return
-	}
-	messageId = m.ID
 }
---
+++ internal/command/command.go
@@ -0,0 +1,149 @@
+package command
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"scurvy10k/internal/models"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/api/cmdroute"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/state"
+	"github.com/diamondburned/arikawa/v3/utils/json/option"
+)
+
+var baseUrl = "https://true.torfstack.com/"
+var debtsUrl = baseUrl + "api/debt"
+
+var channelId discord.ChannelID
+var messageId discord.MessageID
+
+func GetDebts(s *state.State) func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+		debts, err := getDebts()
+		if err != nil {
+			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + err.Error())}
+		}
+		return &api.InteractionResponseData{
+			Embeds: &[]discord.Embed{
+				*transformDebtsToEmbed(debts),
+			},
+		}
+	}
+}
+
+func AddDebt(s *state.State) func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+		options := data.Options
+		name := options.Find("name").String()
+		amount := options.Find("amount").String()
+
+		res, err := http.Post(debtsUrl+"/"+name+"/"+amount, "application/json", nil)
+		if err != nil {
+			log.Error().Msgf("cannot post debt: %s", err)
+			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + err.Error())}
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			log.Error().Msgf("cannot post debt: %s", res.Status)
+			return &api.InteractionResponseData{Content: option.NewNullableString("Error: " + res.Status)}
+		}
+		if channelId != discord.NullChannelID && messageId != discord.NullMessageID {
+			updateDebtsMessage(s)
+		}
+		return &api.InteractionResponseData{
+			Content: option.NewNullableString(fmt.Sprintf("Added %v to %v", amount, name)),
+			Flags:   discord.EphemeralMessage,
+		}
+	}
+}
+
+func SetChannel(s *state.State) func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+		options := data.Options
+		var err error
+		cId, err := options.Find("channel_id").SnowflakeValue()
+		if err != nil {
+			log.Error().Msgf("cannot get channel_id: %s", err)
+			return &api.InteractionResponseData{Content: option.NewNullableString("Could not set channel")}
+		}
+		channelId = discord.ChannelID(cId)
+		debts, err := getDebts()
+		if err != nil {
+			log.Error().Msgf("cannot get debts: %s", err)
+			return &api.InteractionResponseData{Content: option.NewNullableString("Could not get debts")}
+		}
+		m, err := s.SendMessage(discord.ChannelID(channelId), "", *transformDebtsToEmbed(debts))
+		if err != nil {
+			log.Error().Msgf("cannot send message: %s", err)
+			return &api.InteractionResponseData{Content: option.NewNullableString("Could not send message")}
+		}
+		messageId = m.ID
+		return &api.InteractionResponseData{
+			Content: option.NewNullableString("Channel set successfully"),
+			Flags:   discord.EphemeralMessage,
+		}
+	}
+}
+
+func getDebts() (*models.AllDebtsResponse, error) {
+	res, err := http.Get(debtsUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var debts models.AllDebtsResponse
+	if err = json.Unmarshal(bytes, &debts); err != nil {
+		log.Error().Msgf("cannot unmarshal debts: %s", err)
+		return nil, err
+	}
+	return &debts, nil
+}
+
+func transformDebtsToEmbed(debts *models.AllDebtsResponse) *discord.Embed {
+	var fields []discord.EmbedField
+	for _, d := range debts.Debts {
+		fields = append(fields, discord.EmbedField{
+			Name:   d.Name,
+			Value:  d.Amount,
+			Inline: true,
+		})
+	}
+	return &discord.Embed{
+		Title:       "True",
+		Type:        discord.NormalEmbed,
+		Description: "10k in die Gildenbank!",
+		URL:         baseUrl,
+		Timestamp:   discord.NowTimestamp(),
+		Color:       discord.DefaultEmbedColor,
+		Footer: &discord.EmbedFooter{
+			Text: "https://github.com/torfstack/scurvy10k",
+			Icon: "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+		},
+		Fields: fields,
+	}
+}
+
+func updateDebtsMessage(s *state.State) {
+	debts, err := getDebts()
+	if err != nil {
+		log.Error().Msgf("cannot get debts: %s", err)
+		return
+	}
+
+	m, err := s.EditMessage(channelId, messageId, "", *transformDebtsToEmbed(debts))
+	if err != nil {
+		log.Error().Msgf("cannot edit message: %s", err)
+		return
+	}
+	messageId = m.ID
+}
