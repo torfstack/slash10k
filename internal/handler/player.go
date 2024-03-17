6e1ef58285da3729e1f3ffd52e4d45e1af71df88
Max Torf <torfstack@gmail.com>
2024-03-17 19:12:38 +0100

    reworked player handler

--- internal/handler/player.go
+++ internal/handler/player.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 	"scurvy10k/internal/utils"
 	"scurvy10k/sql/db"
 )
@@ -13,12 +14,13 @@ import (
 func AddPlayer(c echo.Context) error {
 	name := c.Param("name")
 	if name == "" {
-		return c.String(400, "Name is required!")
+		return c.String(400, "name is required!")
 	}
 
 	conn, err := utils.GetConnection(utils.DefaultConfig())
 	if err != nil {
-		return c.String(500, "Could not get db connection!")
+		log.Err(err).Msg("could not get db connection!")
+		return c.String(500, "could not get db connection!")
 	}
 	defer func(conn *pgx.Conn, ctx context.Context) {
 		_ = conn.Close(ctx)
@@ -28,7 +30,8 @@ func AddPlayer(c echo.Context) error {
 
 	count, err := q.NumberOfPlayers(context.Background())
 	if err != nil {
-		return c.String(500, "Could not get number of players!")
+		log.Err(err).Msg("could not get number of players!")
+		return c.String(500, "could not get number of players!")
 	}
 	if count >= 100 {
 		return c.String(400, "Max number of players reached!")
@@ -36,6 +39,7 @@ func AddPlayer(c echo.Context) error {
 
 	p, err := q.AddPlayer(context.Background(), name)
 	if err != nil {
+		log.Err(err).Msg("could not add player!")
 		return c.String(500, "Could not add player!")
 	}
 	_, err = q.SetDebt(context.Background(), db.SetDebtParams{
@@ -46,10 +50,12 @@ func AddPlayer(c echo.Context) error {
 		},
 	})
 	if err != nil {
+		log.Err(err).Msg("could not add zero debt to player!")
 		return c.String(500, "Could not add zero debt to player!")
 	}
 
-	return c.String(200, fmt.Sprintf("added player %v", p))
+	log.Info().Msgf("added player %s with id %v", p.Name, p.ID)
+	return c.String(200, fmt.Sprintf("added player %s with id %v", p.Name, p.ID))
 }
 
 func DeletePlayer(c echo.Context) error {
