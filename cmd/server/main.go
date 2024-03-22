ae0fbb26e70052df30b49b0b3f853b1f7af8e8e4
Max Torf <torfstack@gmail.com>
2024-03-22 17:56:19 +0100

    fix: missing /

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -44,7 +44,7 @@ func main() {
 	api.GET("/debt/:player", handler.GetDebt)
 	api.POST("/debt/:player/:amount", handler.AddDebt(d))
 
-	api.GET("journal/:player", handler.GetJournalEntries(d))
+	api.GET("/journal/:player", handler.GetJournalEntries(d))
 
 	admin := api.Group("/admin")
 
